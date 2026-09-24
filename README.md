4110335e8173531122659fa5f51100d2a5d2fd61
marychristine-lb <marychristine.bajao@csucc.edu.ph>
2026-09-24 19:04:17 +0800

    Add the whole rock_paper_scissors program with the README file.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rock, Paper, Scissors (Dart)
+Name: Mary Chrsitine L. Bajao
+Course/Year/Section: BSIT 3B-1 IT 120 - ILDV3
+
+# How to run the program?
+dart rock_paper_scissors.dart
--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 
+///nag import para maka input
+
+///nag initialize ug list nga naay sulod sa mga moves
 const List<String> movesNgaMadawat = ['rock', 'paper', 'scissors'];
 
+///function para makakauha sa name sa player
 String getNameSaPlayer(int numberSaPlayer) {
   stdout.write('Player $numberSaPlayer, Enter your name: ');
   String? inputtedNameSaPlayer = stdin.readLineSync();
@@ -15,11 +19,12 @@ String getNameSaPlayer(int numberSaPlayer) {
   return inputtedNameSaPlayer;
 }
 
+///function para makuha ang move sa player
 String kuhaSaMoveSaPlayer(String ngalanSaPlayer) {
   String? validatedNgaMove;
 
   do {
-    stdout.write('$nameSaPlayer, Enter your move (rock/paper/scissor): ');
+    stdout.write('$ngalanSaPlayer, Enter your move (rock/paper/scissor): ');
     String? inputtedNaMoveSaPlayer = stdin.readLineSync();
 
     validatedNgaMove = validationSaMove(inputtedNaMoveSaPlayer);
@@ -34,6 +39,7 @@ String kuhaSaMoveSaPlayer(String ngalanSaPlayer) {
   return validatedNgaMove;
 }
 
+///function para ivalidate if ang move kay naa sa valid moves
 String? validationSaMove(String? inputtedNaMoveSaPlayer) {
   if (inputtedNaMoveSaPlayer == null) {
     return null;
@@ -46,5 +52,93 @@ String? validationSaMove(String? inputtedNaMoveSaPlayer) {
     return processedNgaInputSaPlayer;
   } else {
     return null;
+  }
+}
+
+///function para ma-identify ang winner base sa move sa mga players
+String? kinsaAngDaog(
+  String moveSaPlayer1,
+  String moveSaPlayer2,
+  String nameSaPlayer1,
+  String nameSaPlayer2,
+) {
+  if (moveSaPlayer1 == moveSaPlayer2) {
+    return null;
+  }
+
+  if ((moveSaPlayer1 == 'rock' && moveSaPlayer2 == 'scissors') ||
+      (moveSaPlayer1 == 'paper' && moveSaPlayer2 == 'rock') ||
+      (moveSaPlayer1 == 'scissors' && moveSaPlayer2 == 'paper')) {
+    return nameSaPlayer1;
+  } else {
+    return nameSaPlayer2;
+  }
+}
+
+///main function
+void main() {
+  print('=== ROCK, PAPER, SCISSORS ===');
+
+  String player1NgaName = getNameSaPlayer(1);
+  String player2NgaName = getNameSaPlayer(2);
+
+  int scoreSaPlayer1 = 0;
+  int scoreSaPlayer2 = 0;
+  int numberSaRound = 1;
+  String playUtro;
+
+  do {
+    print('\n Round $numberSaRound');
+
+    String moveSaPlayer1 = kuhaSaMoveSaPlayer(player2NgaName);
+
+    for (int i = 0; i < 30; i++) {
+      print('');
+    }
+    print('(screen cleared)');
+
+    String moveSaPlayer2 = kuhaSaMoveSaPlayer(player2NgaName);
+
+    print(
+      '\n $player1NgaName chose $moveSaPlayer1 \n $player2NgaName chose $moveSaPlayer2',
+    );
+
+    String? daog = kinsaAngDaog(
+      moveSaPlayer1,
+      moveSaPlayer2,
+      player1NgaName,
+      player2NgaName,
+    );
+
+    if (daog == null) {
+      print('Result: ${daog ?? "It's a Draw!"}');
+    } else {
+      print('Result: $daog Wins the Round!');
+      if (daog == player1NgaName) {
+        scoreSaPlayer1++;
+      } else {
+        scoreSaPlayer2++;
+      }
+    }
+
+    print(
+      'Scores:\n $player1NgaName: $scoreSaPlayer1\n $player2NgaName: $scoreSaPlayer2',
+    );
+
+    stdout.write('Paly Again? (y/n): ');
+    playUtro = stdin.readLineSync()?.trim().toLowerCase() ?? 'n';
+
+    numberSaRound++;
+  } while (playUtro == 'y');
+
+  print('===== FINAL SCORE =====');
+  print('$player1NgaName: $scoreSaPlayer1 \n $player2NgaName: $scoreSaPlayer2');
+
+  if (scoreSaPlayer1 > scoreSaPlayer2) {
+    print('Overall Winner: $player1NgaName');
+  } else if (scoreSaPlayer2 > scoreSaPlayer1) {
+    print('Overall Winner: $player2NgaName');
+  } else {
+    print('Overall Winner: It\'s a Tie!');
   }
 }
